4b5955dae7a44fd1b087761cd26571ecf177c8a4
Chris Han <hxljustdoit@gmail.com>
2018-05-13 17:10:40 +0800

    refactor

--- app.js
+++ app.js
@@ -2,37 +2,24 @@ const http = require('http')
 const path = require('path')
 const fs = require('fs')
 
-const app = http.createServer((req, res) => {
-  if (req.url === '/') {
-    const htmlPath = path.join(__dirname, 'public', 'index.html')
-    fs.readFile(htmlPath, (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-Type': 'text/html'})
-      res.end(data)
-    })
-  } else if (req.url.match('.css$')) {
-    const cssPath = path.join(__dirname, 'public', req.url)
-    fs.readFile(cssPath, (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-Type': 'text/css'})
-      res.end(data)
-    })
-  } else if (req.url.match('.js$')) {
-    const jsPath = path.join(__dirname, 'public', req.url)
-    fs.readFile(jsPath, (err, data) => {
-      if (err) throw err
-      res.writeHead(200, {'Content-Type': 'text/javascript'})
-      res.end(data)
-    })
-  } else {
-    res.writeHead(404)
-    res.write('404 Not Found')
+const staticFile = (req, res) => {
+  const suffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '')
+  const staticPath = path.join(__dirname, 'public', suffix === '/' ? '/index.html' : suffix)
+
+  fs.readFile(staticPath, (err, data) => {
+    if (err) {
+      res.writeHead(404)
+      res.write('404 Not Found')
+      res.end()
+    }
+    res.writeHead(200)
+    res.write(data)
     res.end()
-  }
-})
+  }) 
+}
+
+const app = http.createServer(staticFile)
 
 app.listen(3000, () => {
   console.log('listening on localhost:3000')
 })
-
-console.log('starting')
