e6f198082b24223ac91a5dac4dfa31f1b768395b
Chris Han <hxljustdoit@gmail.com>
2018-05-19 17:57:46 +0800

    add next to my express

--- app.js
+++ app.js
@@ -1,47 +1,59 @@
 const http = require('http')
 const url = require('url')
-const path = require('path')
+const resolve = require('path').resolve
+const send = require('send')
 const fs = require('fs')
 
 const myExpress = () => {
   const middlewares = []
   const express = (req, res) => {
-    middlewares.forEach(middleware => middleware(req, res))
+    middlewares.reduce((prev, next) => {
+      prev(req, res, next)
+      return next
+    })
   }
   express.use = middleware => middlewares.push(middleware)
   return express
 }
 
-const router = (req, res, next) => {
-  req.pathname = url.parse(req.url).pathname
-  switch (req.pathname) {
-    case '/login':
-      req.pathname = 'login.html'
-      break
-    case '/index':
-    case '/':
-      req.pathname = 'index.html'
-      break
-    default:
+const staticFile = (path, options) => {
+  const opts = Object.create(options || null)
+  opts.root = resolve(path)
+
+  return (req, res, next) => {
+    let pathname = url.parse(req.url).pathname
+    const stream = send(req, pathname, opts)
+    stream.on('error', () => {
+      res.root = opts.root
+      next(req, res)
+    })
+    stream.on('file', () => {
+      res.statusCode = 200
+    })
+    stream.pipe(res)
   }
 }
 
-const staticFile = (req, res) => {
-  const staticPath = path.join(__dirname, 'public', req.pathname)
-  const stream = fs.createReadStream(staticPath)
-  stream.on('error', (err) => {
-    res.writeHead(404)
-    res.write('404 Not Found')
-    res.end()
-  })
-  res.statusCode = 200
-  stream.pipe(res)
+const router = (req, res, next) => {
+  const pathname = url.parse(req.url).pathname
+  switch (pathname) {
+    case '/login':
+      send(req, resolve(res.root, 'login.html')).pipe(res)
+      break
+    case '/index':
+      send(req, resolve(res.root, 'index.html')).pipe(res)
+      break
+    default:
+      res.statusCode = 404
+      res.write('404')
+      res.end()
+  }
 }
 
 const app = myExpress()
 
+app.use(staticFile('public'))
 app.use(router)
-app.use(staticFile)
 
 const server = http.createServer(app)
 
