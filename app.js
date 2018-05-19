b1308cd658b3fe4f294718e4b1d9f4a9de21580d
Chris Han <hxljustdoit@gmail.com>
2018-05-19 14:33:48 +0800

    add poor router

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const http = require('http')
+const url = require('url')
 const path = require('path')
 const fs = require('fs')
 
-const staticFile = (req, res) => {
-  const suffix = path.normalize(req.url).replace(/^(\.\.[\/\\])+/, '')
-  const staticPath = path.join(__dirname, 'public', suffix === '/' ? '/index.html' : suffix)
+const router = (req, res) => {
+  let pathname = url.parse(req.url).pathname
+  switch (pathname) {
+    case '/login':
+      pathname = 'login.html'
+      break
+    case '/index':
+    case '/':
+      pathname = 'index.html'
+      break
+    default:
+  }
 
+  staticFile(pathname, res)
+}
+
+const staticFile = (pathname, res) => {
+  const staticPath = path.join(__dirname, 'public', pathname)
   const stream = fs.createReadStream(staticPath)
   stream.on('error', (err) => {
     res.writeHead(404)
@@ -16,7 +31,7 @@ const staticFile = (req, res) => {
   stream.pipe(res)
 }
 
-const app = http.createServer(staticFile)
+const app = http.createServer(router)
 
 app.listen(3000, () => {
   console.log('listening on localhost:3000')
